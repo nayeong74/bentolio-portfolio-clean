a16f0d4aed41930095c5d4e1dd420779aeb7d894
nayeong74 <nayong7434@gmail.com>
2025-05-31 15:12:26 +0900

    WOODZ 분위기에 맞는 음표 애니메이션으로 변경, 이슈 해결

--- petal.js
+++ petal.js
@@ -1,9 +1,14 @@
-// 벚꽃 애니메이션 스크립트
-const petalCount = 30;
-const petals = [];
-const canvas = document.createElement('canvas');
-canvas.className = 'petal-canvas';
-document.body.appendChild(canvas);
+// WOODZ 음악 분위기에 맞는 음표 애니메이션
+const noteCount = 25;
+const notes = [];
+const noteSymbols = ['\u266B','\u266A','\u266C','\u2669'];
+const colors = ['#F8AFA6','#FADCD9','#B2A4FF','#FFC7EA','#FFB4B4'];
+const canvas = document.querySelector('.petal-canvas') || (() => {
+  const c = document.createElement('canvas');
+  c.className = 'petal-canvas';
+  document.body.appendChild(c);
+  return c;
+})();
 const ctx = canvas.getContext('2d');
 
 function resizeCanvas() {
@@ -17,46 +22,46 @@ function random(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-function Petal() {
+function Note() {
   this.x = random(0, canvas.width);
   this.y = random(-canvas.height, 0);
-  this.radius = random(8, 18);
-  this.speed = random(1, 2.5);
-  this.wind = random(-0.5, 0.5);
+  this.size = random(28, 48);
+  this.speed = random(1, 2.3);
+  this.wind = random(-0.8, 0.8);
   this.angle = random(0, Math.PI * 2);
-  this.spin = random(-0.02, 0.02);
+  this.spin = random(-0.01, 0.01);
+  this.symbol = noteSymbols[Math.floor(random(0, noteSymbols.length))];
+  this.color = colors[Math.floor(random(0, colors.length))];
+  this.alpha = random(0.7, 1);
 }
-Petal.prototype.update = function() {
+Note.prototype.update = function() {
   this.x += this.wind;
   this.y += this.speed;
   this.angle += this.spin;
-  if (this.y > canvas.height + 20) {
+  if (this.y > canvas.height + 40) {
     this.x = random(0, canvas.width);
-    this.y = random(-20, 0);
+    this.y = random(-40, 0);
   }
 };
-Petal.prototype.draw = function(ctx) {
+Note.prototype.draw = function(ctx) {
   ctx.save();
   ctx.translate(this.x, this.y);
   ctx.rotate(this.angle);
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  ctx.bezierCurveTo(6, -8, 14, -2, 0, this.radius);
-  ctx.bezierCurveTo(-14, -2, -6, -8, 0, 0);
-  ctx.fillStyle = 'rgba(255,192,203,0.7)';
-  ctx.shadowColor = 'rgba(255,182,193,0.5)';
-  ctx.shadowBlur = 8;
-  ctx.fill();
+  ctx.globalAlpha = this.alpha;
+  ctx.font = `${this.size}px serif`;
+  ctx.fillStyle = this.color;
+  ctx.fillText(this.symbol, 0, 0);
   ctx.restore();
 };
-for (let i = 0; i < petalCount; i++) {
-  petals.push(new Petal());
+notes.length = 0;
+for (let i = 0; i < noteCount; i++) {
+  notes.push(new Note());
 }
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let p of petals) {
-    p.update();
-    p.draw(ctx);
+  for (let n of notes) {
+    n.update();
+    n.draw(ctx);
   }
   requestAnimationFrame(animate);
 }
